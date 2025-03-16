ed4b624d863f3395c490539b616b250749d8ce8a
mikatan-mikan <a200401021236@gmail.com>
2025-03-16 20:20:32 +0900

    interval設定

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 import requests
 import os
+import time
 
 app = Flask(__name__)
 
+ip_cache = {}
+
 # Discord Webhook のURL（環境変数で管理）
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+
+MANY_REQUESTS_INTERVAL = 9
 
 @app.route('/')
 def home():
@@ -16,6 +21,11 @@ def proxy():
     if not DISCORD_WEBHOOK_URL:
         return jsonify({"error": "Webhook URL is not set"}), 500
     
+    # 同じipからrequestsが連続で飛んできてたら
+    if request.remote_addr in ip_cache and time.time() - ip_cache[request.remote_addr] < MANY_REQUESTS_INTERVAL:
+        return jsonify({"error": "Too many requests from this IP"}), 429
+    
+    ip_cache[request.remote_addr] = time.time()
     data = request.json
     headers = {'Content-Type': 'application/json'}
 
